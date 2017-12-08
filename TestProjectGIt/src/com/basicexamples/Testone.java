558e08a1bd51bbe0209e9c7df2afb2d93dcb39a5
ashokkumar <arelly.ashok06@gmail.com>
2017-12-08 02:34:25 -0800

    new statement added

--- TestProjectGIt/src/com/basicexamples/Testone.java
+++ TestProjectGIt/src/com/basicexamples/Testone.java
@@ -5,6 +5,8 @@ public class Testone {
 	public static void main(String[] args) {
 		// Giving a comment here to check changes.
 		System.out.println("This is Test one class Program");
+		
+		System.out.println("New Line Added to this class to test");
 	}
 
 }
